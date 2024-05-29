7609631ab3656b3f2a84f18587fa329f97eb6dd2
Thounny <thounnykeo@gmail.com>
2024-05-28 20:52:29 -0400

    Protect routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) {
+    auth().protect();
+  }
+
+  return NextResponse.next();
+});
+
+const isProtectedRoute = createRouteMatcher([
+  "/",
+]);
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
