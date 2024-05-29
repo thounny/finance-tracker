10221c8800775f60ee7b6d167daa7c41d78ddfbc
Thounny <thounnykeo@gmail.com>
2024-05-28 21:50:55 -0400

    zValidator setup | protected API routes separately

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,29 @@
-import { Hono } from "hono"
-import { handle } from "hono/vercel"
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
-    return c.json({
-        message: 'Hello Next.js!',
+app
+    .get(
+    "/hello", 
+    clerkMiddleware(),
+    (c) => {
+        const auth = getAuth(c);
+        
+        if (!auth?.userId) {
+            return c.json({ error: "Unauthorized" });
+        }
+
+        return c.json({
+            message: 'Hello Next.js!',
+            userId: auth.userId,
+        });
     });
-});
 
 export const GET = handle(app);
 export const POST = handle(app);
