90764dd26cb7089647dad43b6cf7487f50b908f0
Thounny <thounnykeo@gmail.com>
2024-05-29 15:26:56 -0400

    Navigation buttons, Sheet + React-use  installation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,4 +1,7 @@
-"use client";
+"use client"; // server side render
+
+import { usePathname } from "next/navigation";
+import { NavButton } from "./nav-button";
 
 const routes = [
     {
@@ -24,10 +27,17 @@ const routes = [
 ]
 
 export const Navigation = () => {
+    const pathname = usePathname();
+
     return (
         <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
             {routes.map((route) => (
-                <p>{route.label}</p>
+                <NavButton
+                    key={route.href}
+                    href={route.href}
+                    label={route.label}
+                    isActive={pathname === route.href }
+                />
             ))}
         </nav>
     )
