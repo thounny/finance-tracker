970ab8f51e08742a80e16a3e99110e20c1fa1a5f
Thounny <thounnykeo@gmail.com>
2024-05-28 21:18:38 -0400

    Sign out feature

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
+import { UserButton } from "@clerk/nextjs"
+
 export default function Home() {
   return (
-    <p>
-      This is an authenticated route.
-   </p>
+    <UserButton afterSignOutUrl="/" />
   )
 }
