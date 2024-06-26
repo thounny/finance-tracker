b1ef4cadd9868e6c4216cdf4e8d743c180de18b6
Thounny <thounnykeo@gmail.com>
2024-06-26 17:13:56 -0400

    eq from drizzle-orm

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -3,6 +3,7 @@ import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { HTTPException } from "hono/http-exception";
+import { eq } from "drizzle-orm";
 
 const app = new Hono()
     .get(
@@ -22,7 +23,8 @@ const app = new Hono()
                     id: accounts.id,
                     name: accounts.name,
                 })
-                .from(accounts);
+                .from(accounts)
+                .where(eq(accounts.userId, auth.userId));
             
             return c.json({ data });
         });
