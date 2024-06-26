7bb5c7fc6825dc22fefb9102927b60bf379d73ea
Thounny <thounnykeo@gmail.com>
2024-06-26 16:11:20 -0400

    Drizzle ORM and Neon Database

---
+++ db/drizzle.ts
@@ -0,0 +1,7 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+export const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql);
+
+
---
+++ scripts/migrate.ts
@@ -0,0 +1,20 @@
+import { config } from "dotenv";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { migrate } from "drizzle-orm/neon-http/migrator"
+
+config({ path: ".env.local" });
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql);
+
+const main = async () => {
+    try {
+        await migrate(db, { migrationsFolder: "drizzle" });
+    } catch (error) {
+        console.error("Error during migration:", error);
+        process.exit(1);
+    }
+};
+
+main();
