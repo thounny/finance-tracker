3b2b9c7b49e1e56c4065448258db0cc92a00f9e1
Thounny <thounnykeo@gmail.com>
2024-06-26 17:07:37 -0400

    type safety = HTTPException

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,17 +1,30 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono()
-    .get("/", async (c) => {
-        const data = await db
-            .select({
-                id: accounts.id,
-                name: accounts.name,
-            })
-            .from(accounts);
-        
-        return c.json({ data });
-    });
+    .get(
+        "/",
+        clerkMiddleware(),
+        async (c) => {
+            const auth = getAuth(c);
+
+            if (!auth?.userId) {
+                throw new HTTPException(401, {
+                    res: c.json({ error: "Unauthorized" }, 401),
+                });
+            }
+
+            const data = await db
+                .select({
+                    id: accounts.id,
+                    name: accounts.name,
+                })
+                .from(accounts);
+            
+            return c.json({ data });
+        });
 
 export default app;
