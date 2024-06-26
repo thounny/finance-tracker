1e2c83aad9cf482a60bf826c23aec2c1432d96e2
Thounny <thounnykeo@gmail.com>
2024-06-26 17:18:30 -0400

    removed HTTPException | new hono fixes 401 status

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -2,7 +2,6 @@ import { Hono } from "hono";
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { HTTPException } from "hono/http-exception";
 import { eq } from "drizzle-orm";
 
 const app = new Hono()
@@ -13,9 +12,7 @@ const app = new Hono()
             const auth = getAuth(c);
 
             if (!auth?.userId) {
-                throw new HTTPException(401, {
-                    res: c.json({ error: "Unauthorized" }, 401),
-                });
+                return c.json({ error: "Unauthorized" }, 401);
             }
 
             const data = await db
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,19 +1,10 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import accounts from "./accounts";
-import { HTTPException } from "hono/http-exception";
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
-
-app.onError((err, c) => {
-    if (err instanceof HTTPException) {
-        return err.getResponse();
-    }
-
-    return c.json({ error: "Internal error" }, 500);
-});
 
 const routes = app
     .route("/accounts", accounts);
