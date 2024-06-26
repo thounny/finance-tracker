6db6407bac7ed75950a5c602cd4b3db3d2c65b29
Thounny <thounnykeo@gmail.com>
2024-06-26 16:55:03 -0400

    type safety = error return

--- features/accounts/api/use-get-accounts.ts
+++ features/accounts/api/use-get-accounts.ts
@@ -7,6 +7,15 @@ export const useGetAccounts = () => {
         queryKey: ["accounts"],
         queryFn: async () => {
             const response = await client.api.accounts.$get();
-        }
-    })
-}
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch accounts");
+            }
+
+            const { data } = await response.json();
+            return data;
+        },
+    });
+
+    return query;
+};
