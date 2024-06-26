25d3b1b1de429ecad468c4fa3b80d404d0e07525
Thounny <thounnykeo@gmail.com>
2024-06-26 16:24:59 -0400

    Drizzle migration | plaid id setup

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,10 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import { accounts } from "./schema";
 
 export const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql);
+
+const accounts2 = db.select().from(accounts)
 
 
--- db/schema.ts
+++ db/schema.ts
@@ -2,6 +2,7 @@ import { pgTable, text } from "drizzle-orm/pg-core";
 
 export const accounts = pgTable("accounts", {
     id: text("id").primaryKey(),
+    plaidId: text("plaid_id"),
     name: text("name").notNull(),
     userId: text("user_id").notNull(),
 });
