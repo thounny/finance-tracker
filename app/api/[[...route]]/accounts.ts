d23ab67a1e22dcee61b4122d11669efde3b2ff2f
Thounny <thounnykeo@gmail.com>
2024-06-26 16:44:14 -0400

    React-query setup

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,17 @@
 import { Hono } from "hono";
+import { db } from "@/db/drizzle";
+import { accounts } from "@/db/schema";
 
 const app = new Hono()
-    .get("/", (c) => {
-        return c.json({ accounts: [] });
+    .get("/", async (c) => {
+        const data = await db
+            .select({
+                id: accounts.id,
+                name: accounts.name,
+            })
+            .from(accounts);
+        
+        return c.json({ data });
     });
 
 export default app;
