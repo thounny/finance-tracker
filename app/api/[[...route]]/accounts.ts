fff774d2b4589d7785f47c6fdda985ec57868faa
Thounny <thounnykeo@gmail.com>
2024-06-27 16:45:52 -0400

    paralleldrive cuid install | Accounts POST API

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { eq } from "drizzle-orm";
+import { zValidator } from "@hono/zod-validator";
+import { createId } from "@paralleldrive/cuid2"
 
 const app = new Hono()
     .get(
@@ -24,6 +26,29 @@ const app = new Hono()
                 .where(eq(accounts.userId, auth.userId));
             
             return c.json({ data });
+        })
+    .post(
+        "/",
+        clerkMiddleware(),
+        zValidator("json", insertAccountSchema.pick({
+            name: true,
+
+        })),
+        async (c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid("json");
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const data = await db.insert(accounts).values({
+                id: createId(),
+                userId: auth.userId,
+                ...values,
+            })
+
+            return c.json({});
         });
 
 export default app;
