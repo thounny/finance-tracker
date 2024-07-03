dc216bd9f94a26bb9a34d24d90841cded12ac553
Thounny <thounnykeo@gmail.com>
2024-07-03 19:54:56 -0400

    loading table

--- app/(dashboard)/accounts/columns.tsx
+++ app/(dashboard)/accounts/columns.tsx
@@ -1,20 +1,15 @@
 "use client"
 
+import { InferResponseType } from "hono"
 import { Button } from "@/components/ui/button"
 import { ColumnDef } from "@tanstack/react-table"
 import { ArrowUp, ArrowUpDown } from "lucide-react"
 import { Checkbox } from "@/components/ui/checkbox"
+import {client} from "@/lib/hono"
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
-export type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
-}
+export type ResponseType = InferResponseType<typeof client.api.accounts.$get, 200>["data"][0];
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<ResponseType>[] = [
   {
     id: "select",
     header: ({ table }) => (
@@ -38,25 +33,17 @@ export const columns: ColumnDef<Payment>[] = [
     enableHiding: false,
   },
   {
-    accessorKey: "status",
-    header: "Status",
-  },
-  {
-    accessorKey: "email",
+    accessorKey: "name",
     header: ({ column }) => {
       return (
         <Button
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          Email
+          Name
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       )
     }
-  },
-  {
-    accessorKey: "amount",
-      header: "Amount",
   },
 ]
--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -8,27 +8,33 @@ import {
     CardTitle,
 } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
-import { Plus } from "lucide-react";
-import { Payment, columns } from "./columns";
+import { Loader2, Plus } from "lucide-react";
+import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
-
-const data: Payment[] = [
-        {
-            id: "728ed52f",
-            amount: 100,
-            status: "pending",
-            email: "m@example.com",
-    },
-    {
-            id: "728ed52f",
-            amount: 2,
-            status: "success",
-            email: "a@example.com",
-        },
-    ]
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const AccountsPage = () => {
     const newAccount = useNewAccount();
+    const accountsQuery = useGetAccounts();
+    const accounts = accountsQuery.data || [];
+
+    if (accountsQuery.isLoading) {
+        return (
+            <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader>
+                        <Skeleton className="h-8 w-48" />
+                    </CardHeader>
+                    <CardContent>
+                        <div className="h-[500px] w-full flex items-center justify-center">
+                            <Loader2 className="size-6 text-slate-300 animate-spin" />
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        );
+    }
 
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -46,7 +52,7 @@ const AccountsPage = () => {
                     <DataTable
                         filterKey="email"
                         columns={columns}
-                        data={data}
+                        data={accounts}
                         onDelete={() => { }}
                         disabled={false}
                     />
