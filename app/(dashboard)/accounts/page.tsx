a53bb166a6128be8cf73c1853515cad7ef70eee9
Thounny <thounnykeo@gmail.com>
2024-07-03 19:28:57 -0400

    reusable data table

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -9,6 +9,23 @@ import {
 } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
 import { Plus } from "lucide-react";
+import { Payment, columns } from "./columns";
+import { DataTable } from "@/components/data-table";
+
+const data: Payment[] = [
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "m@example.com",
+    },
+    {
+            id: "728ed52f",
+            amount: 2,
+            status: "success",
+            email: "a@example.com",
+        },
+    ]
 
 const AccountsPage = () => {
     const newAccount = useNewAccount();
@@ -25,6 +42,15 @@ const AccountsPage = () => {
                         Add new
                     </Button>
                 </CardHeader>
+                <CardContent>
+                    <DataTable
+                        filterKey="email"
+                        columns={columns}
+                        data={data}
+                        onDelete={() => { }}
+                        disabled={false}
+                    />
+                </CardContent>
             </Card>
         </div>
     );
