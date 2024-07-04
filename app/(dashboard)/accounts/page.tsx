2519bfd1c5ff7917d0919add893750d9c08558ee
Thounny <thounnykeo@gmail.com>
2024-07-03 20:14:28 -0400

    accounts bulk delete | some errors with ids string

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -13,11 +13,17 @@ import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete";
 
 const AccountsPage = () => {
     const newAccount = useNewAccount();
+    const deleteAccounts = useBulkDeleteAccounts();
     const accountsQuery = useGetAccounts();
     const accounts = accountsQuery.data || [];
+
+    const isDisabled =
+        accountsQuery.isLoading ||
+        deleteAccounts.isPending;
 
     if (accountsQuery.isLoading) {
         return (
@@ -53,8 +59,11 @@ const AccountsPage = () => {
                         filterKey="email"
                         columns={columns}
                         data={accounts}
-                        onDelete={() => { }}
-                        disabled={false}
+                        onDelete={(row) => {
+                            const ids = row.map((r) => r.original.id);
+                            deleteAccounts.mutate({ ids });
+                        }}
+                        disabled={isDisabled}
                     />
                 </CardContent>
             </Card>
--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -2,9 +2,10 @@ import { Hono } from "hono";
 import { db } from "@/db/drizzle";
 import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 import { zValidator } from "@hono/zod-validator";
 import { createId } from "@paralleldrive/cuid2"
+import { z } from "zod";
 
 const app = new Hono()
     .get(
@@ -49,6 +50,38 @@ const app = new Hono()
             }).returning();
 
             return c.json({ data });
-        });
+        })
+    .post(
+        "/bulk-delete",
+        clerkMiddleware(),
+        zValidator(
+            "json",
+            z.object({
+                ids: z.array(z.string()),
+            }),
+        ),
+        async (c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid("json");
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const data = await db
+                .delete(accounts)
+                .where(
+                    and(
+                        eq(accounts.userId, auth.userId),
+                        inArray(accounts.id, values.ids)
+                    )
+                )
+                .returning({
+                    id: accounts.id,
+                });
+            
+            return c.json({ data });
+        },
+    );
 
 export default app;
