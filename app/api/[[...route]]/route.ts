c32aae7554defb084b6d05dfd70c446724ed0675
Thounny <thounnykeo@gmail.com>
2024-07-16 18:51:03 -0400

    transactions API

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import accounts from "./accounts";
 import categories from "./categories";
+import transactions from "./transactions";
 
 export const runtime = 'edge';
 
@@ -9,7 +10,8 @@ const app = new Hono().basePath('/api')
 
 const routes = app
     .route("/accounts", accounts)
-    .route("/categories", categories);
+    .route("/categories", categories)
+    .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
