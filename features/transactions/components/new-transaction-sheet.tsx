0774a78dd95cec01e84dffed7ba62df32366ac8c
Thounny <thounnykeo@gmail.com>
2024-07-19 13:41:09 -0400

    transactions form | TODO fix route for transactions and category

---
+++ features/transactions/components/new-transaction-sheet.tsx
@@ -0,0 +1,102 @@
+import { z } from "zod";
+import { insertTransactionSchema } from "@/db/schema";
+import { 
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+} from "@/components/ui/sheet";
+import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
+import { useCreateTransaction } from "../api/use-create-transaction";
+import { useCreateCategory } from "@/features/categories/api/use-create-category";
+import { useGetCategories } from "@/features/categories/api/use-get-categories";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useCreateAccount } from "@/features/accounts/api/use-create-account";
+import { TransactionForm } from "./transaction-form";
+import { Loader2 } from "lucide-react";
+
+const formSchema = insertTransactionSchema.omit({
+    id: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+export const NewTransactionSheet = () => {
+    const { isOpen, onClose } = useNewTransaction();
+
+    const createMutation = useCreateTransaction();
+
+    const categoryQuery = useGetCategories();
+    const categoryMutation = useCreateCategory();
+    const onCreateCategory = (name: string) => categoryMutation.mutate({
+        name
+    });
+    const categoryOptions = (categoryQuery.data ?? []).map((category) => ({
+        label: category.name,
+        value: category.id,
+    }));
+
+    const accountQuery = useGetAccounts();
+
+    const accountMutation = useCreateAccount();
+
+    const onCreateAccount = (name: string) => categoryMutation.mutate({
+        name
+    });
+
+    const accountOptions = (accountQuery.data ?? []).map((account) => ({
+        label: account.name,
+        value: account.id,
+    }));
+    
+    const isPending =
+        createMutation.isPending ||
+        categoryMutation.isPending ||
+        accountMutation.isPending;
+    
+    const isLoading =
+        categoryQuery.isLoading ||
+        accountQuery.isLoading;
+
+    const onSubmit = (values: FormValues) => {
+        createMutation.mutate(values, {
+            onSuccess: () => {
+                onClose();
+            },
+        });
+    };
+
+    return (
+        <Sheet open={isOpen} onOpenChange={onClose}>
+            <SheetContent className="space-y-4">
+                <SheetHeader>
+                    <SheetTitle>
+                        New Transaction
+                    </SheetTitle>
+                    <SheetDescription>
+                        Add a new transaction
+                    </SheetDescription>
+                </SheetHeader>
+                {isLoading
+                    ? (
+                        <div className="absolute inset-0 flex items-center
+                        justify-center">
+                            <Loader2 className="size-4 text-muted-foreground animate-spin"/>
+                        </div>
+                    )
+                    : (
+                <TransactionForm
+                    onSubmit={(onSubmit)}
+                    disabled={isPending}
+                    categoryOptions={categoryOptions}
+                    onCreateCategory={onCreateCategory}
+                    accountOptions={accountOptions}
+                    onCreateAccount={onCreateAccount}
+                />
+                )
+            }
+            </SheetContent>
+        </Sheet>
+    );
+};
--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -9,6 +9,9 @@ import { EditAccountSheet } from "@/features/accounts/components/edit-account-sh
 import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
 
 import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
+
+import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
+
 export const SheetProvider = () => {
     const isMounted = useMountedState();
 
@@ -21,6 +24,8 @@ export const SheetProvider = () => {
 
             <NewCategorySheet />
             <EditCategorySheet />
+
+            <NewTransactionSheet />
         </>
     );
 };
