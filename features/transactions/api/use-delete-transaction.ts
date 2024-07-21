0d2b90ac19cc544b6277fcd19b9cd5a8bbe6d7fa
Thounny <thounnykeo@gmail.com>
2024-07-21 15:37:54 -0400

    modified transactions

--- features/transactions/api/use-delete-transaction.ts
+++ features/transactions/api/use-delete-transaction.ts
@@ -1,5 +1,5 @@
 import { toast } from "sonner";
-import { InferRequestType, InferResponseType } from "hono";
+import { InferResponseType } from "hono";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { client } from "@/lib/hono";
